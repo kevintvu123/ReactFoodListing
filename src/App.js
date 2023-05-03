5e3e3bc2f0e8299919b15a02ab910e4b8c7dc42c
Kevin Vu <kevintvu123@gmail.com>
2023-05-03 16:14:23 -0400

    updated mock API fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-
+import { useState, useEffect } from "react";
+import useMockFoodData from "./util/MockFoodData";
 
 function App() {
+  const foodData = useMockFoodData()
+
+  console.log(foodData)
+
   return (
     <div className="app">
     </div>
---
+++ src/util/MockFoodData.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+
+function useMockFoodData() {
+    const [foodData, setFoodData] = useState([])
+
+    // Hardcoded JSON data
+    const mockFoodData = [
+        {
+            "Name": "Juicy Juice",
+            "Type": "Drink",
+            "Category": [
+                "Cold",
+                "Fruit"
+            ],
+            "Image": "*plugin any image url here*",
+            "Description": "It’s quite juicy.",
+            "Expiration": "2025-06-23T23:00:00Z"
+
+        },
+        {
+            "Name": "Hot Potato",
+            "Type": "Food",
+            "Category": [
+                "Hot",
+                "Potato"
+            ],
+            "Image": "*plugin any image url here*",
+            "Description": "It’s quite hot.",
+            "Expiration": "2025-06-23T23:00:00Z"
+        },
+        {
+            "Name": "Cold Potato",
+            "Type": "Food",
+            "Category": [
+                "Cold",
+                "Potato"
+            ],
+            "Image": "*plugin any image url here*",
+            "Description": "It’s quite cold.",
+            "Expiration": "2023-03-23T23:00:00Z"
+        }
+    ]
+
+    // Mock querying from an API
+    useEffect(() => {
+        const fetchFoods = async () => {
+            // Figured I would add a commented out structure for a fetch
+            // const response = await fetch("/api/foods");
+            // const data = await response.json();
+            // setFoodData(data);
+            setFoodData(mockFoodData)
+        }
+        fetchFoods()
+    }, [])
+
+    return foodData
+}
+
+export default useMockFoodData
